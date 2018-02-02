3e42ba2cc8e4fb30eb1c8e926896de3dff8e7ce9
mdemuynck <maarten.demuynck@loqutus.com>
2018-02-02 11:31:01 +0100

    add express

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-var rc = require("./piswitch");
+var rc = require("piswitch");
+var express = require("express");
 
-var code = [];
-code["A"] = "0FFF0 0FFFF 0F";
-code["B"] = "0FFF0 F0FFF 0F";
-code["C"] = "0FFF0 FF0FF 0F";
-code["D"] = "0FFF0 FFF0F 0F";
-
+var app = express();
 var off = false;
 
+app.get('/', function(req, resp){
+    var s = req.query.socket;
+    toggleSocket(s, off);
+    off = true;
+    resp.send("socket "+ s + "on");
+});
 
-var type = "tristate"
+app.listen(8001);
 
-rc.send(code, type, off);
+function toggleSocket(s, off){
+    var code = [];
+    code["A"] = "0FFF0 0FFFF 0F";
+    code["B"] = "0FFF0 F0FFF 0F";
+    code["C"] = "0FFF0 FF0FF 0F";
+    code["D"] = "0FFF0 FFF0F 0F";
+
+    var setOff = off;
+
+    var type = "tristate"
+
+    rc.send(code, type, setOff);
+}
