e95104ec3d02149e20e8fb3577b32eb97e7018c0
mdemuynck <maarten.demuynck@loqutus.com>
2018-02-02 11:12:23 +0100

    add mock-require

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var rc = require("./piswitch");
 
-var code = 
+var code = [];
+code["A"] = "0FFF0 0FFFF 0F";
+code["B"] = "0FFF0 F0FFF 0F";
+code["C"] = "0FFF0 FF0FF 0F";
+code["D"] = "0FFF0 FFF0F 0F";
+
+var off = false;
+
+
 var type = "tristate"
 
-rc.send(code, type);
+rc.send(code, type, off);
